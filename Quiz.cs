920adf21bb453c2c9eee6f89cfb54044ba16a301
seyren10 <gagarinroyvictor@gmail.com>
2021-07-06 22:09:14 +0800

    changes dictionary to property

--- Quiz.cs
+++ Quiz.cs
@@ -4,28 +4,17 @@ using System.Collections.Generic;
 
 public class Quiz
 {
-  public Letter answer {get;set;}
-  public string question {get;set;}
-
-  private Dictionary<Letter,string> _choices = new Dictionary<Letter,string>();
-
-  public void SetChoices(Letter letter, string option)
-  {
-    _choices.Add(letter,option);
-  }
-
-  public Dictionary<Letter,string> GetChoices()
-  {
-    return _choices;
-  }
+    public Letter answer { get; set; }
+    public string question { get; set; }
+    public Dictionary<Letter, string> choices { get; set; }
 }
 
 public enum Letter
 {
-  a,
-  b,
-  c,
-  d
+    a,
+    b,
+    c,
+    d
 }
 
 
